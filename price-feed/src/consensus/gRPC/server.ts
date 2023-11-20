79756dce53c8de86cfb046d5d2b427d20e4e33a0
ngocban284 <daongocban284@gmail.com>
2023-11-20 17:15:02 +0700

    integration oracle to gRPC

--- price-feed/src/consensus/gRPC/server.ts
+++ price-feed/src/consensus/gRPC/server.ts
@@ -1,14 +1,58 @@
-import path from 'path';
-
 import {
   Server,
   ServerCredentials,
   loadPackageDefinition,
 } from '@grpc/grpc-js';
+import path from 'path';
 import { loadSync } from '@grpc/proto-loader';
+import OracleTestController from '../../controllers/oracle_gRPC';
+import { Ticker } from '../../domain/ticker';
+import { PriceSourceManager } from '../../domain/price-source';
+import { BitfinexPriceSource } from '../../ports/bitfinex';
+import { CoingeckoPriceSource } from '../../ports/coingecko';
+import { KrakenPriceSource } from '../../ports/kraken';
+import { OkxPriceSource } from '../../ports/okx';
+import { BinancePriceSource } from '../../ports/binance';
+import { ECPairFactory } from 'ecpair';
+import * as ecc from 'tiny-secp256k1';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
+// oracle price sources (median will be applied)
+const sources = [
+  new BitfinexPriceSource(),
+  new CoingeckoPriceSource(),
+  new KrakenPriceSource(),
+  new OkxPriceSource(),
+  new BinancePriceSource(),
+];
+
+if (!PRIVATE_KEY) {
+  console.error('Missing PRIVATE_KEY env var');
+  process.exit(1);
+}
+
+const ECPair = ECPairFactory(ecc);
+const oraclePair = ECPair.fromPrivateKey(Buffer.from(PRIVATE_KEY, 'hex'));
+
+const oracleController = new OracleTestController(
+  oraclePair,
+  [
+    Ticker.BTCUSD,
+    Ticker.ETHUSD,
+    Ticker.LINKUSD,
+    Ticker.DOTUSD,
+    Ticker.ADAUSD,
+    Ticker.XRPUSD,
+  ],
+  new PriceSourceManager(sources, console.error),
+  IS_DEVELOPMENT
+);
 
 const protoPath = path.join(__dirname, '/proto/helloworld.proto');
-
 const packageDefinition = loadSync(protoPath, {
   keepCase: true,
   longs: String,
@@ -21,6 +65,7 @@ const helloWorldProto: any =
   loadPackageDefinition(packageDefinition).helloworld;
 
 function sayHello(call: any, callback: any) {
+  console.log('oracleController: ', oracleController.getInfo());
   callback(null, { message: 'Hello ' + call.request.name });
 }
 
---
+++ price-feed/src/controllers/oracle_gRPC.ts
@@ -0,0 +1,118 @@
+import * as ecc from 'tiny-secp256k1';
+import { Get, Route, Tags, Path, Query } from 'tsoa';
+import crypto from 'crypto';
+import { ECPairInterface } from 'ecpair';
+import { uint64LE } from '../utils/bufferutils';
+import { PriceSource } from '../domain/price-source';
+import { Ticker } from '../domain/ticker';
+import { filterMessageTo32 } from '../utils/bufferutils';
+import * as fs from 'fs';
+import secp256k1 from 'secp256k1';
+import { randomBytes } from 'crypto';
+import keccak256 from 'keccak256';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export type OracleTestAttestation = {
+  timestamp: string;
+  lastPrice: string;
+  attestation: {
+    signature: string;
+    message: string;
+    messageHash: string;
+    validatorAddress?: string;
+  };
+};
+
+export type OracleTestInfo = {
+  publicKey: string;
+  availableTickers: string[];
+};
+
+export default class OracleTestController {
+  constructor(
+    private keyPair: ECPairInterface,
+    private availableTickers: string[],
+    private priceSource: PriceSource,
+    private isDevelopment: boolean = false
+  ) {}
+
+  public getInfo(): OracleTestInfo {
+    return {
+      publicKey: this.keyPair.publicKey.toString('hex'),
+      availableTickers: this.availableTickers,
+    };
+  }
+
+  private getTimestampNowMs(): number {
+    return Math.trunc(Date.now());
+  }
+
+  private isTickerAvailable(ticker: string): ticker is Ticker {
+    return this.availableTickers.includes(ticker);
+  }
+
+  public async getAttestationForTicker(
+    ticker: string,
+    timestamp: string,
+    lastPrice: string
+  ): Promise<OracleTestAttestation | null> {
+    if (!this.isTickerAvailable(ticker)) return null;
+
+    // DEVELOPMENT ONLY: provide timestamp and lastPrice via querystring to "simulate" the oracle signing the message
+    let timestampToUse = Number(timestamp);
+    let lastPriceToUse = Number(lastPrice);
+
+    // PRODUCTION: use the price source to get the timestamp and last price
+    if (!this.isDevelopment) {
+      const price = await this.priceSource.getPrice(ticker);
+      timestampToUse = this.getTimestampNowMs();
+      lastPriceToUse = Math.trunc(price);
+    }
+
+    try {
+      const timpestampLE64 = uint64LE(timestampToUse);
+      const priceLE64 = uint64LE(lastPriceToUse);
+      const iso4217currencyCode = Buffer.from(ticker.replace('BTC', ''));
+      const message = Buffer.from([
+        ...timpestampLE64,
+        ...priceLE64,
+        ...iso4217currencyCode,
+      ]);
+
+      const hash = crypto
+        .createHash('sha256')
+        .update(filterMessageTo32(message))
+        .digest();
+      if (!this.keyPair.privateKey) throw new Error('No private key found');
+      const sig = secp256k1.ecdsaSign(
+        filterMessageTo32(message),
+        this.keyPair.privateKey
+      );
+
+      // get wallet address from private key
+      const privateKey = Buffer.from(process.env.PRIVATE_KEY!, 'hex');
+      const publicKey = secp256k1.publicKeyCreate(privateKey);
+
+      // decompress public key and use keccak256 to get the address
+      const decompressedKey = secp256k1.publicKeyConvert(publicKey, false);
+      const address = keccak256(Buffer.from(decompressedKey.slice(1)))
+        .slice(-20)
+        .toString('hex');
+
+      return {
+        timestamp: timestampToUse!.toString(),
+        lastPrice: lastPriceToUse!.toString(),
+        attestation: {
+          signature: Buffer.from(sig.signature).toString('hex'),
+          message: message.toString('hex'),
+          messageHash: hash.toString('hex'),
+          validatorAddress: '0x' + address,
+        },
+      };
+    } catch (e) {
+      console.error(e);
+      throw new Error('Bitfinex: An error occurred while signing the message');
+    }
+  }
+}
